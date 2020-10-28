ba14f3e6eb4ada4ef5c16b7dab03e20c0cba4347
Mirko Westermeier <mirko@westermeier.de>
2020-10-29 00:15:25 +0100

    Add nav titles where they are needed

--- md-party.js
+++ md-party.js
@@ -18,9 +18,10 @@ new Vue({
             <nav>
                 <label for="nav-burger" id="nav-burger-icon">&#9776;</label>
                 <input type="checkbox" id="nav-burger">
+                <span id="nav-burger-title">{{ config.title }}</span>
 
                 <div id="nav-items">
-                    <span class="nav-title">{{ config.title }}</span>
+                    <span id="nav-items-title">{{ config.title }}</span>
                     <a
                         v-for="page in sitemap"
                         :key="page"
