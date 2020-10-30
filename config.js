aa9eae76f8719aff7ac2ce8cc667d2bdfb23b9c1
Mirko Westermeier <mirko@westermeier.de>
2020-10-30 11:36:35 +0100

    Add explanations to the config file

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 var mdPartyConfig = {
 
+    // Regarding the markdown source pages...
     "pages": {
+
+        // Defines the file name of a YAML file. This YAML file defines the
+        // list of pages and their order.
         "sitemapYaml":  "sitemap.yml",
+
+        // Defines the markdown source directory. This can be a simple local
+        // directory or a full URL, for example to raw github file resources.
         "fetchPrefix":  "Content"
     },
 
+    // A title for the website. Appears in the title and in the navigation bar.
     "title": "md-party"
 };
